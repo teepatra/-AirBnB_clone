97f7a5c3f3663e8e5f84ae5ae2a7f3d1519c27dc
sapera11 <akinsetewilliams@gmail.com>
2023-08-09 13:35:20 -0700

    updated

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""State Class
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State Class
+    Args:
+        BaseModel ([class]): [class inherited by State]
+    """
+    name = ""
